79eab8411456cc80a25d70daac17fae3bb6c452b
abdenour <bareche.abdenour@gmail.com>
2020-01-13 01:54:49 +0100

    Updates

---
+++ precache-manifest.54b9028814876dd4c5213130e490f13f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a0df0c75f1465a51c48461dd2244221",
+    "url": "/mynaur/react-app/index.html"
+  },
+  {
+    "revision": "f3fd37122c06f58dac4f",
+    "url": "/mynaur/react-app/static/css/main.b59dfb5e.chunk.css"
+  },
+  {
+    "revision": "cb86b9d208f16ed300fe",
+    "url": "/mynaur/react-app/static/js/2.e2c89c95.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/mynaur/react-app/static/js/2.e2c89c95.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f3fd37122c06f58dac4f",
+    "url": "/mynaur/react-app/static/js/main.e2ce59a2.chunk.js"
+  },
+  {
+    "revision": "0079019af88f4f5f8803",
+    "url": "/mynaur/react-app/static/js/runtime-main.3e9ed416.js"
+  }
+]);
